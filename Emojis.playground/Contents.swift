d882c5582d9e0dd8e83805b17eb9a901906d1715
Alexsa Tolentino <adt7577@nyu.edu>
2023-02-13 00:47:09 -0500

    Emojis

---
+++ Emojis.playground/Contents.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+var myMood = "😛"
+var favoriteFood = "🍔"
+var myHobby = "📸"
+
+let welcome_Interpolated = "Most of the time I feel \(myMood) which makes me validate my UBER spending on \(favoriteFood) but if I am not eating, you can probably find me \(myHobby) people"
+print (welcome_Interpolated)
+
+
+
+
